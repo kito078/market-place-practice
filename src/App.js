e8612b192ddfd63822979f03fcc14f5cc03f51df
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-09-24 16:44:24 +0200

    w

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="explore" element={<Explore />} />
+        <Route path="offers" element={<Offers />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
